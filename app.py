f7555e49d84132954ef339aace2bac4b0b8fd94a
Juanan Jiménez <juananjimenez1975@gmail.com>
2022-11-18 11:40:08 +0100

    con correcciones

--- app.py
+++ app.py
@@ -1,56 +1,68 @@
-from flask import Flask, request, render_template, redirect, url_for, jsonify, abort
+from flask import Flask, render_template, request, redirect, url_for, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import sys
 
 app = Flask(__name__)
-
-app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:abc@localhost:5432/todoapp'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:abc@localhost:5432/todoapp'
 db = SQLAlchemy(app)
-
-with app.app_context():
-    db.init_app(app)
-    #db.create_all()
-
 migrate = Migrate(app, db)
 
 class Todo(db.Model):
-    __tablename__ = 'todos'
-    id = db.Column(db.Integer, primary_key=True)
-    descripcion= db.Column(db.String(), nullable=False)
-    completado = db.Column(db.Boolean, nullable=True, default= False)
+  __tablename__ = 'todos'
+  id = db.Column(db.Integer, primary_key=True)
+  description = db.Column(db.String(), nullable=False)
+  completed = db.Column(db.Boolean, nullable=False)
+
+  def __repr__(self):
+    return f'<Todo {self.id} {self.description}>'
+
+# note: more conventionally, we would write a
+# POST endpoint to /todos for the create endpoint:
+# @app.route('/todos', method=['POST'])
+@app.route('/todos/create', methods=['POST'])
+def create_todo():
+  error = False
+  body = {}
+  try:
     
-def __repr__(self):
-    return f'<Todo ID: {self.id}, descripcion: {self.descripcion}>'
+    description = request.get_json()['description']
+    todo = Todo(description=description, completed=False)
+    db.session.add(todo)
+    db.session.commit()
+    body['id'] = todo.id
+    body['completed'] = todo.completed
+    body['description'] = todo.description
+  except:
+    error = True
+    db.session.rollback()
+    print(sys.exc_info())
+  finally:
+    db.session.close()
+  if error:
+    abort (400)
+  else:
+    return jsonify(body)
+
+@app.route('/todos/<todo_id>/set-completed', methods=['POST'])
+def set_completed_todo(todo_id):
+  try:
+    completed = request.get_json()['completed']
+    print('completed', completed)
+    todo = Todo.query.get(todo_id)
+    todo.completed = completed
+    db.session.commit()
+  except:
+    db.session.rollback()
+  finally:
+    db.session.close()
+  return redirect(url_for('index'))
 
 @app.route('/')
 def index():
-    return render_template('index.html', data=Todo.query.all())
+  return render_template('index.html', todos=Todo.query.order_by('id').all())
 
-@app.route('/todos/create', methods=['Post'])
-def create_todo():
-    body={}
-    error = False
-    try:
-        descripcion  = request.get_json()['descripcion']
-        todo = Todo(descripcion = descripcion)
-        body['descripcion'] = todo.descripcion
-        db.session.add(todo)
-        db.session.commit()
-    except:
-        error == True
-        db.session.rollback()
-        print(sys.exec_info())
-    finally:
-        db.session.close()
-        if error == True:
-            abort(400)
-        else:
-            return jsonify(body)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
+    app.debug = True
     app.run(host="0.0.0.0", port=5000)
-    
-
