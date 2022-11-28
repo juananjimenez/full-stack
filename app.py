884cf01853a224c083d9ddc0ff483d4b388aa740
Juanan Jiménez <juananjimenez1975@gmail.com>
2022-11-28 16:08:12 +0100

    errores varios

--- app.py
+++ app.py
@@ -11,11 +11,22 @@ migrate = Migrate(app, db)
 class Todo(db.Model):
   __tablename__ = 'todos'
   id = db.Column(db.Integer, primary_key=True)
-  description = db.Column(db.String(), nullable=False)
-  completed = db.Column(db.Boolean, nullable=False)
+  descripcion = db.Column(db.String(), nullable=False)
+  completado = db.Column(db.Boolean, nullable=False, default=False)
+  list_id = db.Column(db.Integer, db.ForeignKey('todolist.id'), nullable=False)
 
-  def __repr__(self):
-    return f'<Todo {self.id} {self.description}>'
+def __repr__(self):
+  return f'<Todo {self.id} {self.descripcion}, list {self.list_id}>'
+
+class TodoList(db.Model):
+  __tablename__ = 'todolist' 
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(), nullable=False)
+  todos = db.relationship('Todo', backref='list', lazy=True)
+  
+def __repr__(self):
+    return f'<TodoList {self.id} {self.name}>'
+  
 
 # note: more conventionally, we would write a
 # POST endpoint to /todos for the create endpoint:
@@ -24,15 +35,14 @@ class Todo(db.Model):
 def create_todo():
   error = False
   body = {}
-  try:
-    
-    description = request.get_json()['description']
-    todo = Todo(description=description, completed=False)
+  try: 
+    descripcion = request.get_json()['descripcion']
+    todo = Todo(descripcion=descripcion, completed=False)
     db.session.add(todo)
     db.session.commit()
     body['id'] = todo.id
-    body['completed'] = todo.completed
-    body['description'] = todo.description
+    body['completado'] = todo.completado
+    body['descripcion'] = todo.descripcion
   except:
     error = True
     db.session.rollback()
@@ -47,10 +57,10 @@ def create_todo():
 @app.route('/todos/<todo_id>/set-completed', methods=['POST'])
 def set_completed_todo(todo_id):
   try:
-    completed = request.get_json()['completed']
-    print('completed', completed)
+    completado = request.get_json()['completado']
+    print('completado', completado)
     todo = Todo.query.get(todo_id)
-    todo.completed = completed
+    todo.completado = completado
     db.session.commit()
   except:
     db.session.rollback()
@@ -58,9 +68,21 @@ def set_completed_todo(todo_id):
     db.session.close()
   return redirect(url_for('index'))
 
+@app.route('/todos/<todo_id>', methods=['DELETE'])
+def delete_todo(todo_id):
+  try:
+    Todo.query.filter_by(id=todo_id).delete()
+    db.session.commit()
+  except:
+    db.session.rollback()
+  finally:
+    db.session.close()
+  return jsonify({'success': True})
+
+
 @app.route('/')
 def index():
-  return render_template('index.html', todos=Todo.query.order_by('id').all())
+  return render_template('index.html', todo=Todo.query.order_by('id').all())
 
 
 if __name__ == '__main__':
